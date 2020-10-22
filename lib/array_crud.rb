1b3a160fb34000349875ed1ec257f4de473693c2
Zach Bayard <zbayard@gmail.com>
2020-10-22 19:56:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+array = ["wow"]
+array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
